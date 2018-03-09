c40e2a32d71697ac29d2b75898dbc57ec4a22e34
Micah Chiang <micahkchiang@gmail.com>
2018-03-08 19:16:59 -0800

    paldindrome recurse

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -22,3 +22,28 @@ var longestPalindrome = function(s) {
 
 // Given a string, find and return the longest substring that is also a palindrome. 
 // Don't use this, it isn't efficient at all...
+
+// Work in Progress. I wasn't necessarily happy with the above solution, so I wanted to approach from a different angle.
+// The below is not finished, as there are still some cases that need handling (mainly how left and right are used). 
+// It uses recursion. Sorry it's a little sloppy.
+// I've been in a class all day, and haven't had much time to sit down and actually code.
+
+var longestPalindrome = function(s) {
+    if(s.length < 2) return s;
+    return palindromeRecurse(s,0,s.length);
+}
+
+function palindromeRecurse(s,left,right) {
+    var str = s.substring(left,right);
+    if(isPalindrome(str)) {
+        return str;
+    }
+    if(left < right) {
+        return palindromeRecurse(str,left+1,right-1);
+    }
+}
+
+function isPalindrome(s) {
+    let reverse = s.split('').reverse().join('');
+    return reverse === s;
+}
